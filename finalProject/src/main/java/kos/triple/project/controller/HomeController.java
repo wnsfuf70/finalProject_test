7768f21366f5daed5563291d7fed7297de87db05
KO_11 <KO_11@KO_11-PC>
2018-01-02 22:00:56 +0900

    SQL 로그출력완료

--- finalProject/src/main/java/kos/triple/project/controller/HomeController.java
+++ finalProject/src/main/java/kos/triple/project/controller/HomeController.java
@@ -7,7 +7,6 @@ import java.util.Locale;
 import javax.inject.Inject;
 
 import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
