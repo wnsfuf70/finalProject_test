ce80f3d0352ea8a634f47fd0b77242a4e306c433
wnsfuf70 <wnsfuf70@gmail.com>
2018-01-06 01:38:14 +0900

    리소스테스트

--- finalProject/src/main/java/kos/triple/project/controller/HomeController.java
+++ finalProject/src/main/java/kos/triple/project/controller/HomeController.java
@@ -40,18 +40,7 @@ public class HomeController {
 		return "home";
 	}
 	
-	@RequestMapping(value = "testAction", method = RequestMethod.GET)
-	public String testAction(Locale locale, Model model) {
-		
-		service.sqlTest();
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		String formattedDate = dateFormat.format(date);
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
-	}
+	
 	
 	
 	
